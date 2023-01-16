27c251ae37939da8722d6ecb45a3c03de9b5a7dd
jc01rho <rkjnice@gmail.com>
2023-01-16 17:35:57 +0900

    fixed  imports

--- selfdrive/car/gm/radar_interface.py
+++ selfdrive/car/gm/radar_interface.py
@@ -3,7 +3,7 @@ import math
 from cereal import car
 from common.conversions import Conversions as CV
 from opendbc.can.parser import CANParser
-from selfdrive.car.gm.values import DBC, CanBus
+from selfdrive.car.gm.values import CAR, DBC, CanBus
 from selfdrive.car.interfaces import RadarInterfaceBase
 
 RADAR_HEADER_MSG = 1120
